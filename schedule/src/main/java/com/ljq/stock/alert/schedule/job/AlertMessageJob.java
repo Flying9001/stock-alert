0c34861f6f51c9dca1114ab234bf91527b751a61
Flying9001 <flying9001@gmail.com>
2025-05-09 17:18:05 +0800

    1.优化定时任务异常处理机制

--- schedule/src/main/java/com/ljq/stock/alert/schedule/job/AlertMessageJob.java
+++ schedule/src/main/java/com/ljq/stock/alert/schedule/job/AlertMessageJob.java
@@ -61,21 +61,24 @@ public class AlertMessageJob {
     @Async(value = TaskExecutorConfig.STOCK_ALERT_SCHEDULE_NAME)
     @Scheduled(fixedDelay = 5 * 1000L, initialDelay = 5 * 1000L)
     public void flashStockData() {
-        // 从缓存中读取所有股票数据
-        List<StockSourceEntity> stockCacheList = redisUtil.mapGetAll(StockConst.CACHE_KEY_STOCK_SOURCE_ALL,
-                StockSourceEntity.class);
-        if (CollUtil.isEmpty(stockCacheList)) {
-            return;
+        try {
+            // 从缓存中读取所有股票数据
+            List<StockSourceEntity> stockCacheList = redisUtil.mapGetAll(StockConst.CACHE_KEY_STOCK_SOURCE_ALL,
+                    StockSourceEntity.class);
+            if (CollUtil.isEmpty(stockCacheList)) {
+                return;
+            }
+            // 查询股票数据
+            List<StockSourceEntity> stockLiveList = StockUtil.getStocksLive(stockApiConfig, stockCacheList);
+            // 更新缓存中股票数据
+            Map<String, Object> stockSourceMap = new HashMap<>(16);
+            stockLiveList.forEach(stockSource ->
+                    stockSourceMap.put(CacheKeyUtil.createStockSourceKey(stockSource.getMarketType(),
+                            stockSource.getStockCode()), stockSource));
+            redisUtil.mapPutBatch(StockConst.CACHE_KEY_STOCK_SOURCE_ALL, stockSourceMap);
+        } catch (Exception e) {
+            log.error("定时刷新股票异常", e);
         }
-        log.debug("测试定时任务是否停止");
-        // 查询股票数据
-        List<StockSourceEntity> stockLiveList = StockUtil.getStocksLive(stockApiConfig, stockCacheList);
-        // 更新缓存中股票数据
-        Map<String, Object> stockSourceMap = new HashMap<>(16);
-        stockLiveList.forEach(stockSource ->
-                stockSourceMap.put(CacheKeyUtil.createStockSourceKey(stockSource.getMarketType(),
-                        stockSource.getStockCode()), stockSource));
-        redisUtil.mapPutBatch(StockConst.CACHE_KEY_STOCK_SOURCE_ALL, stockSourceMap);
     }
 
     /**
@@ -85,24 +88,28 @@ public class AlertMessageJob {
     @Async(value = TaskExecutorConfig.STOCK_ALERT_SCHEDULE_NAME)
     @Scheduled(fixedDelay = 10 * 1000L, initialDelay = 30 * 1000L)
     public void compareStockPrice() {
-        // 查询所有用户关注的股票
-        int countAll = userStockDao.selectCount(Wrappers.emptyWrapper());
-        int pageSize = 1000;
-        int times = countAll % pageSize == 0 ? countAll / pageSize : (countAll / pageSize) + 1;
-        IPage<UserStockEntity> page = new Page<>(1,pageSize);
-        for (int i = 1; i < times + 1; i++) {
-           page.setCurrent(i);
-           page = userStockDao.queryPage(Collections.emptyMap(), page);
-            // 创建预警消息
-            if (CollUtil.isEmpty(page.getRecords())) {
-                continue;
+        try {
+            // 查询所有用户关注的股票
+            int countAll = userStockDao.selectCount(Wrappers.emptyWrapper());
+            int pageSize = 1000;
+            int times = countAll % pageSize == 0 ? countAll / pageSize : (countAll / pageSize) + 1;
+            IPage<UserStockEntity> page = new Page<>(1,pageSize);
+            for (int i = 1; i < times + 1; i++) {
+               page.setCurrent(i);
+               page = userStockDao.queryPage(Collections.emptyMap(), page);
+                // 创建预警消息
+                if (CollUtil.isEmpty(page.getRecords())) {
+                    continue;
+                }
+                // 获取用户关注股票的实时价格
+                page.getRecords().forEach(userStock ->
+                    userStock.setStockSource(redisUtil.mapGet(StockConst.CACHE_KEY_STOCK_SOURCE_ALL,
+                            CacheKeyUtil.createStockSourceKey(userStock.getStockSource().getMarketType(),
+                                    userStock.getStockSource().getStockCode()), StockSourceEntity.class)));
+                createAndSendMessageBatch(page.getRecords());
             }
-            // 获取用户关注股票的实时价格
-            page.getRecords().forEach(userStock ->
-                userStock.setStockSource(redisUtil.mapGet(StockConst.CACHE_KEY_STOCK_SOURCE_ALL,
-                        CacheKeyUtil.createStockSourceKey(userStock.getStockSource().getMarketType(),
-                                userStock.getStockSource().getStockCode()), StockSourceEntity.class)));
-            createAndSendMessageBatch(page.getRecords());
+        } catch (Exception e) {
+            log.error("定时比对股票异常", e);
         }
 
     }
@@ -114,30 +121,34 @@ public class AlertMessageJob {
     @Async(value = TaskExecutorConfig.STOCK_ALERT_SCHEDULE_NAME)
     @Scheduled(fixedDelay = 120 * 1000L, initialDelay = 30 * 1000L)
     public void messageReTry() {
-        // 统计所有当天发送失败的消息
-        int pageSize = 1000;
-        IPage<AlertMessageEntity> pageParam = new Page<>(1, pageSize);
-        IPage<AlertMessageEntity> pageResult = alertMessageDao.queryPageFailMessage(pageParam);
-        if (pageResult.getTotal() < 1) {
-            return;
-        }
-        long countAll = pageResult.getTotal();
-        long times = countAll % pageSize == 0 ? countAll / pageSize : (countAll / pageSize) + 1;
-        StringBuilder messageBuilder = new StringBuilder("retry alert message: \n");
-        pageResult.getRecords().forEach(alertMessage -> messageBuilder.append("id=").append(alertMessage.getId())
-                .append(",title=").append(alertMessage.getTitle()).append("\n"));
-        log.info("{}", messageBuilder);
-        alertMessageMqSender.sendBatchAlertMessage(pageResult.getRecords());
-        for (int i = 2; i < times + 1; i++) {
-            pageParam.setCurrent(i);
-            pageResult = alertMessageDao.queryPageFailMessage(pageParam);
-            if (CollUtil.isEmpty(pageResult.getRecords())) {
-                continue;
+        try {
+            // 统计所有当天发送失败的消息
+            int pageSize = 1000;
+            IPage<AlertMessageEntity> pageParam = new Page<>(1, pageSize);
+            IPage<AlertMessageEntity> pageResult = alertMessageDao.queryPageFailMessage(pageParam);
+            if (pageResult.getTotal() < 1) {
+                return;
             }
+            long countAll = pageResult.getTotal();
+            long times = countAll % pageSize == 0 ? countAll / pageSize : (countAll / pageSize) + 1;
+            StringBuilder messageBuilder = new StringBuilder("retry alert message: \n");
             pageResult.getRecords().forEach(alertMessage -> messageBuilder.append("id=").append(alertMessage.getId())
                     .append(",title=").append(alertMessage.getTitle()).append("\n"));
             log.info("{}", messageBuilder);
             alertMessageMqSender.sendBatchAlertMessage(pageResult.getRecords());
+            for (int i = 2; i < times + 1; i++) {
+                pageParam.setCurrent(i);
+                pageResult = alertMessageDao.queryPageFailMessage(pageParam);
+                if (CollUtil.isEmpty(pageResult.getRecords())) {
+                    continue;
+                }
+                pageResult.getRecords().forEach(alertMessage -> messageBuilder.append("id=").append(alertMessage.getId())
+                        .append(",title=").append(alertMessage.getTitle()).append("\n"));
+                log.info("{}", messageBuilder);
+                alertMessageMqSender.sendBatchAlertMessage(pageResult.getRecords());
+            }
+        } catch (Exception e) {
+            log.error("定时消息重试异常", e);
         }
     }
 
@@ -147,33 +158,37 @@ public class AlertMessageJob {
     @Async(value = TaskExecutorConfig.STOCK_ALERT_SCHEDULE_NAME)
     @Scheduled(cron = "0 0 17 ? * 5")
     public void weekReport() {
-        // 查询所有有关注股票的用户(分页分段)
-        int countAll = userInfoDao.queryCountWithStock();
-        int pageSize = 1000;
-        int times = countAll % pageSize == 0 ? countAll / pageSize : (countAll / pageSize) + 1;
-        IPage<UserInfoEntity> page = new Page<>(1,pageSize);
-        // 查询每个用户关注的股票
-        for (int i = 1; i < times + 1; i++) {
-            page.setCurrent(i);
-            page = userInfoDao.queryPageWithStock(page);
-            if (CollUtil.isEmpty(page.getRecords())) {
-                continue;
+        try {
+            // 查询所有有关注股票的用户(分页分段)
+            int countAll = userInfoDao.queryCountWithStock();
+            int pageSize = 1000;
+            int times = countAll % pageSize == 0 ? countAll / pageSize : (countAll / pageSize) + 1;
+            IPage<UserInfoEntity> page = new Page<>(1,pageSize);
+            // 查询每个用户关注的股票
+            for (int i = 1; i < times + 1; i++) {
+                page.setCurrent(i);
+                page = userInfoDao.queryPageWithStock(page);
+                if (CollUtil.isEmpty(page.getRecords())) {
+                    continue;
+                }
+                List<AlertMessageEntity> alertMessageList = new ArrayList<>();
+                page.getRecords().forEach(user -> {
+                    log.info("股价报告接收用户昵称:{},id:{},邮箱:{}",user.getNickName(),user.getId(), user.getEmail());
+                    // 以用户为单位创建推送消息
+                    List<UserStockEntity> userStockList = userStockDao.queryByUser(user.getId());
+                    userStockList.forEach(userStock ->
+                            userStock.setStockSource(redisUtil.mapGet(StockConst.CACHE_KEY_STOCK_SOURCE_ALL,
+                                    CacheKeyUtil.createStockSourceKey(userStock.getStockSource().getMarketType(),
+                                            userStock.getStockSource().getStockCode()), StockSourceEntity.class))
+                    );
+                    AlertMessageEntity alertMessage = MessageHelper.createReportMessage(userStockList);
+                    alertMessageList.add(alertMessage);
+                });
+                // 批量推送消息
+                alertMessageMqSender.sendBatchReportMessage(alertMessageList);
             }
-            List<AlertMessageEntity> alertMessageList = new ArrayList<>();
-            page.getRecords().forEach(user -> {
-                log.info("股价报告接收用户昵称:{},id:{},邮箱:{}",user.getNickName(),user.getId(), user.getEmail());
-                // 以用户为单位创建推送消息
-                List<UserStockEntity> userStockList = userStockDao.queryByUser(user.getId());
-                userStockList.forEach(userStock ->
-                        userStock.setStockSource(redisUtil.mapGet(StockConst.CACHE_KEY_STOCK_SOURCE_ALL,
-                                CacheKeyUtil.createStockSourceKey(userStock.getStockSource().getMarketType(),
-                                        userStock.getStockSource().getStockCode()), StockSourceEntity.class))
-                );
-                AlertMessageEntity alertMessage = MessageHelper.createReportMessage(userStockList);
-                alertMessageList.add(alertMessage);
-            });
-            // 批量推送消息
-            alertMessageMqSender.sendBatchReportMessage(alertMessageList);
+        } catch (Exception e) {
+            log.error("定时推送周报异常", e);
         }
     }
 
